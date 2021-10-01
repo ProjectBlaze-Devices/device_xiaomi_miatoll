22b7ea984f5c518f52a0241f104d0a6d782108d3
kubersharma001 <kubersharma001@gmail.com>
2021-10-01 14:33:04 +0000

    miatoll: Set build fp,desc at build time
     * As we use same fp for all variants it doesnt makes sense to set it via init, lets
    move it out to product makefile
    
    Change-Id: I1674d68a9b9b8611d75212a2d2dfeb5c3f9df3a9
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- arrow_miatoll.mk
+++ arrow_miatoll.mk
@@ -24,3 +24,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Maintainer
 DEVICE_MAINTAINER := SonalSingh18
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
