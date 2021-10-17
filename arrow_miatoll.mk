6a72039330263bd38858f6479b1c44dc6a439964
Sonal Singh <sonal.singh.19993@gmail.com>
2021-10-17 03:36:59 +0200

    miatoll: Switch to walleye FP and Desc
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- arrow_miatoll.mk
+++ arrow_miatoll.mk
@@ -26,6 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 DEVICE_MAINTAINER := SonalSingh18
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
