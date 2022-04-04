12329dc4e74786dbcca2b833d31cb54d552f8a47
Sonal Singh <sonal.singh.19993@gmail.com>
2022-04-04 12:32:09 +0000

    miatoll: Add vendorsetup.sh
     * Just for the sake of ease as roomservice doesnt wrks in unofficial
       build. Although local_manifests is an option but this seems easier.

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+git clone git@github.com:ArrowOS-Devices/android_kernel_xiaomi_sm6250.git -b arrow-12.1 kernel/xiaomi/sm6250
+git clone git@github.com:ArrowOS-Devices/android_device_xiaomi_sm6250-common.git -b arrow-12.1 device/xiaomi/sm6250-common
+git clone git@github.com:ArrowOS-Devices/android_vendor_xiaomi_miatoll.git -b arrow-12.1 vendor/xiaomi/miatoll
+git clone git@github.com:ArrowOS-Devices/android_vendor_xiaomi_sm6250-common.git -b arrow-12.1 vendor/xiaomi/sm6250-common
+git clone git@github.com:ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
+git clone git@github.com:ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt.git -b arrow-12.1 packages/apps/GCamGOPrebuilt
+git clone git@github.com:ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.1 hardware/xiaomi
+git clone --depth=1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b -b master prebuilts/clang/host/linux-x86/clang-r437112b
