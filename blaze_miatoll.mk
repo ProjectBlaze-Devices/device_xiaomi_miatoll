50561c1290d95af6dd2c5c2ba82ee498c9052f6c
SKORPION29 <sayansarkar29.slg@gmail.com>
2022-05-03 01:35:10 +0530

    miatoll: Add Blaze Official Stuff
    Signed-off-by: SKORPION29 <sayansarkar29.slg@gmail.com>

--- blaze_miatoll.mk
+++ blaze_miatoll.mk
@@ -15,6 +15,11 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 $(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
+# Blaze Official Stuff
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := Sayan Sarkar(@SKORPION_29)
+TARGET_USE_PIXEL_CHARGER := true
+
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
