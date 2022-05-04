fc324086cba7795a7abc7041e28408b49b89e036
SKORPION29 <sayansarkar29.slg@gmail.com>
2022-05-04 13:29:45 +0000

    miatoll: Update FP to raven May
    Signed-off-by: SKORPION29 <sayansarkar29.slg@gmail.com>

--- blaze_miatoll.mk
+++ blaze_miatoll.mk
@@ -30,6 +30,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220505.002 8353555 release-keys"
 
-BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys
