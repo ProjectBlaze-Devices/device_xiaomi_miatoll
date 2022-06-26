a2bf0574ff2f8c145d6d55c4f05e24bebb19cb4c
SKORPION29 <sayansarkar29.slg@gmail.com>
2022-06-26 14:36:07 +0530

    miatoll: Update maintainer name for Blaze

--- blaze_miatoll.mk
+++ blaze_miatoll.mk
@@ -17,7 +17,7 @@ $(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Blaze Official Stuff
 BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := SayanSarkar(@SKORPION_29)
+BLAZE_MAINTAINER := SKORPION_29
 TARGET_USE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
